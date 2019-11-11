58942180b8548a5abe4446116f1ac058ab22d2bc
Надя <>
2019-11-11 12:25:57 +0300

    delete extra comments

--- modules/task_2/zhbanova_n_data_network_topology_ring/data_network_topology_ring.cpp
+++ modules/task_2/zhbanova_n_data_network_topology_ring/data_network_topology_ring.cpp
@@ -57,15 +57,9 @@ std::vector<int> sendVector(int rankFrom, int rankTo, std::vector<int> vec) {
   }
 
   if (rank == rankFrom) {   // if it is a sender process
-    // std::cout << "DEBUG: " << "Source vector: ";
-    // for (int i = 0; i < vec.size(); i++) { std::cout << vec[i] << " "; }
-    // std::cout << std::endl;
     sendByRing(vec, rank, size);
   } else if (rank == rankTo) {   // if it is a recipient process
     resultVec = recvFromRing(rank, size);
-    // std::cout << "DEBUG: " << "Result vector: ";
-    // for (int i = 0; i < resultVec.size(); i++) { std::cout << resultVec[i] << " "; }
-    // std::cout << std::endl;
   } else if (rankFrom < rankTo && rank > rankFrom && rank < rankTo) {   // 0 proc ->..-> N proc, when rankFrom < rankTo
     resultVec = recvFromRing(rank, size);
     sendByRing(resultVec, rank, size);
@@ -86,7 +80,6 @@ void sendByRing(std::vector<int> vec, int rank, int size) {
   MPI_Pack(&vec[0], vecSize, MPI_INT, buff, MAX_BUFF_SIZE, &pos, MPI_COMM_WORLD);   // pack the vector itself
   // (rank + 1) % size - recipient process number
   MPI_Send(buff, pos, MPI_PACKED, (rank + 1) % size, 0, MPI_COMM_WORLD);
-  // std::cout << "DEBUG: " << rank << " has sent data to " << ((rank + 1) % size) << std::endl;
 }
 
 std::vector<int> recvFromRing(int rank, int size) {
@@ -100,89 +93,7 @@ std::vector<int> recvFromRing(int rank, int size) {
   MPI_Unpack(buff, MAX_BUFF_SIZE, &pos, &vecSize, 1, MPI_INT, MPI_COMM_WORLD);
   vec.resize(vecSize);
   MPI_Unpack(buff, MAX_BUFF_SIZE, &pos, &vec[0], vecSize, MPI_INT, MPI_COMM_WORLD);
-  // std::cout << "DEBUG: " << rank << " has recieved data from " << (rank - 1 + size) % size << std::endl;
   return vec;
 }
 
-/************2 way*************/
-/*
-void sendVector2(int rankTo, std::vector<int> vec) {   // this function is used only by the sender process
-  int rank;
-  int size;
-  std::vector<int> resultVec;
-
-  MPI_Comm_size(MPI_COMM_WORLD, &size);
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-
-  if (rankTo < 0 || rankTo >= size) {
-    throw std::runtime_error("Error: wrong process TO numer");
-  }
-  if (rank == rankTo) {
-    throw std::runtime_error("Error: request to send data to itself.");
-  }
-
-  // std::cout << "DEBUG2: " << "Source vector: ";
-  // for (int i = 0; i < vec.size(); i++) { std::cout << vec[i] << " "; }
-  // std::cout << std::endl;
-  sendByRing2(vec, rank, rank, rankTo, size);
-}
-
-std::vector<int> transferVector2(int& rankFrom, int& rankTo) {
-  int rank;
-  int size;
-  std::vector<int> vec(VEC_SIZE);
-
-  MPI_Comm_size(MPI_COMM_WORLD, &size);
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-
-  vec = recvFromRing2(rank, size, rankFrom, rankTo);
-  if (rankTo == rank) {   // if vector has reached the recipient, then the recipient send a dummy message
-    std::vector<int> dummyVec;
-    int dummyRankTo = -1;
-    // std::cout << "DEBUG2: " << "Result vector: ";
-    // for (int i = 0; i < vec.size(); i++) { std::cout << vec[i] << " "; }
-    // std::cout << std::endl;
-    // std::cout << "DEBUG2: dummy message has been sent." << std::endl;
-    if (rank != (rankFrom - 1 + size) % size) {   // if the next process is not sender process
-      sendByRing2(dummyVec, rank, rankFrom, dummyRankTo, size);
-    }
-  } else if (rank != (rankFrom - 1 + size) % size) {
-    sendByRing2(vec, rank, rankFrom, rankTo, size);
-  }
-  return vec;
-}
-
-void sendByRing2(std::vector<int> vec, int rank, int rankFrom, int rankTo, int size) {
-  int pos = 0;
-  char buff[MAX_BUFF_SIZE];
-  int vecSize = vec.size();
-
-  MPI_Pack(&rankFrom, 1, MPI_INT, buff, MAX_BUFF_SIZE, &pos, MPI_COMM_WORLD);   // initial process
-  MPI_Pack(&rankTo, 1, MPI_INT, buff, MAX_BUFF_SIZE, &pos, MPI_COMM_WORLD);   // final process
-  if (rankTo != -1) {   // if it is not a dummy message
-    MPI_Pack(&vecSize, 1, MPI_INT, buff, MAX_BUFF_SIZE, &pos, MPI_COMM_WORLD);   // how much we pack
-    MPI_Pack(&vec[0], vec.size(), MPI_INT, buff, MAX_BUFF_SIZE, &pos, MPI_COMM_WORLD);   // pack the vector itself
-  }
-
-  MPI_Send(buff, pos, MPI_PACKED, (rank + 1) % size, 0, MPI_COMM_WORLD);
-  // std::cout << "DEBUG2: " << rank << " has sent data to " << ((rank + 1) % size) << std::endl;
-}
-
-std::vector<int> recvFromRing2(int rank, int size, int& rankFrom, int& rankTo) {
-  int pos = 0;
-  char buff[MAX_BUFF_SIZE];
-  int vecSize;
-  std::vector<int>vec;
-  MPI_Status status;   // received message parameter
-  MPI_Recv(buff, MAX_BUFF_SIZE, MPI_PACKED, (rank - 1 + size) % size, 0, MPI_COMM_WORLD, &status);
-  MPI_Unpack(buff, MAX_BUFF_SIZE, &pos, &rankFrom, 1, MPI_INT, MPI_COMM_WORLD);
-  MPI_Unpack(buff, MAX_BUFF_SIZE, &pos, &rankTo, 1, MPI_INT, MPI_COMM_WORLD);
-  if (rankTo != -1) {   // if it is not a dummy message
-    MPI_Unpack(buff, MAX_BUFF_SIZE, &pos, &vecSize, 1, MPI_INT, MPI_COMM_WORLD);
-    vec.resize(vecSize);
-    MPI_Unpack(buff, MAX_BUFF_SIZE, &pos, &vec[0], vecSize, MPI_INT, MPI_COMM_WORLD);
-  }
-  // std::cout << "DEBUG2: " << rank << " has recieved data from " << (rank - 1 + size) % size << std::endl;
-  return vec;
-} */
 
--- modules/task_2/zhbanova_n_data_network_topology_ring/data_network_topology_ring.h
+++ modules/task_2/zhbanova_n_data_network_topology_ring/data_network_topology_ring.h
@@ -11,15 +11,9 @@ const int VEC_SIZE = 3;
 std::vector<int> randomInputVector(int size);
 std::vector<int> transferDataToCheck(int rank, int rankFrom, int rankTo, std::vector<int> vec);
 
-// 1 way - all processes initially know the recipient process
+// all processes initially know the recipient process
 std::vector<int> sendVector(int rankFrom, int rankTo, std::vector<int> vec);
 void sendByRing(std::vector<int> vec, int rank, int size);
 std::vector<int> recvFromRing(int rank, int size);
-
-// 2 way - only the process sender knows the process recipient, initially
-// void sendVector2(int rankTo, std::vector<int> vec);
-// std::vector<int> transferVector2(int& rankFrom, int& rankTo);
-// void sendByRing2(std::vector<int> vec, int rank, int rankFrom, int rankTo, int size);
-// std::vector<int> recvFromRing2(int rank, int size, int& rankFrom, int& rankTo);
 
 #endif  // MODULES_TASK_2_ZHBANOVA_N_DATA_NETWORK_TOPOLOGY_RING_DATA_NETWORK_TOPOLOGY_RING_H_
