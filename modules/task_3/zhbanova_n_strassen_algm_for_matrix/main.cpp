337eaad16e9a3f43bdd96de10baa3e22514f1b65
Надя <>
2019-12-17 15:49:02 +0300

    ini-1

--- modules/task_3/zhbanova_n_strassen_algm_for_matrix/main.cpp
+++ modules/task_3/zhbanova_n_strassen_algm_for_matrix/main.cpp
@@ -46,119 +46,118 @@ TEST(strassen_algm_for_matrix, check_Strassen_algorithm) {
   }
 }
 
-TEST(strassen_algm_for_matrix, check_Strassen_alg_parall)  {
-	int rank, size;
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	MPI_Comm_size(MPI_COMM_WORLD, &size);
-	if (rank == 0) {
-		int N = 4;
-		double* matr_a = MemoryVectorMatrix(N);
-		double* matr_b = MemoryVectorMatrix(N);
-
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				matr_a[i * N + j] = ((double)i + 1);
-				matr_b[i * N + j] = ((double)i + 1);
-			}
-		}
-		double* matr_trivial = Trivial_alghorithm(matr_a, matr_b, N);
-		double* matr_strassen_parall = Strassen_alg_parall(matr_a, matr_b, N);
-		for (int k = 0; k < N; k++)
-			for (int l = 0; l < N; l++)
-				ASSERT_TRUE(matr_strassen_parall[k * N + l] == matr_trivial[k * N + l]);
-	}
+TEST(strassen_algm_for_matrix, check_Strassen_alg_parall) {
+  int rank, size;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+  if (rank == 0) {
+    int N = 4;
+    double* matr_a = MemoryVectorMatrix(N);
+    double* matr_b = MemoryVectorMatrix(N);
+    for (int i = 0; i < N; i++) {
+      for (int j = 0; j < N; j++) {
+        matr_a[i * N + j] = ((double)i + 1);
+        matr_b[i * N + j] = ((double)i + 1);
+      }
+    }
+    double* matr_trivial = Trivial_alghorithm(matr_a, matr_b, N);
+    double* matr_strassen_parall = Strassen_alg_parall(matr_a, matr_b, N);
+    for (int k = 0; k < N; k++)
+      for (int l = 0; l < N; l++)
+        ASSERT_TRUE(matr_strassen_parall[k * N + l] == matr_trivial[k * N + l]);
+  }
 }
 
 TEST(strassen_algm_for_matrix, check_func_Add_two_matrix) {
-	int size;
-	MPI_Comm_size(MPI_COMM_WORLD, &size);
-	if (size < 2) {
-		int N = 4;
-		double* matr_a = MemoryVectorMatrix(N);
-		double* matr_b = MemoryVectorMatrix(N);
-		double* matr_c = MemoryVectorMatrix(N);
+  int size;
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+  if (size < 2) {
+    int N = 4;
+    double* matr_a = MemoryVectorMatrix(N);
+    double* matr_b = MemoryVectorMatrix(N);
+    double* matr_c = MemoryVectorMatrix(N);
 
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				matr_a[i * N + j] = ((double)i + 1);
-				matr_b[i * N + j] = ((double)i + 1);
-				matr_c[i * N + j] = (((double)i + 1) *2);
-			}
-		}
-		double* matr_add2 = Add2(matr_a, matr_b, N);
-		for (int k = 0; k < N; k++)
-			for (int l = 0; l < N; l++)
-				ASSERT_TRUE(matr_c[k * N + l] == matr_add2[k * N + l]);
-	}
+    for (int i = 0; i < N; i++) {
+      for (int j = 0; j < N; j++) {
+        matr_a[i * N + j] = ((double)i + 1);
+        matr_b[i * N + j] = ((double)i + 1);
+        matr_c[i * N + j] = (((double)i + 1) *2);
+      }
+    }
+    double* matr_add2 = Add2(matr_a, matr_b, N);
+    for (int k = 0; k < N; k++)
+      for (int l = 0; l < N; l++)
+        ASSERT_TRUE(matr_c[k * N + l] == matr_add2[k * N + l]);
+  }
 }
 
 TEST(strassen_algm_for_matrix, check_func_Add_four_matrix) {
-	int size;
-	MPI_Comm_size(MPI_COMM_WORLD, &size);
-	if (size < 2) {
-		int N = 4;
-		double* matr_a = MemoryVectorMatrix(N);
-		double* matr_b = MemoryVectorMatrix(N);
-		double* matr_c = MemoryVectorMatrix(N);
+  int size;
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+  if (size < 2) {
+    int N = 4;
+    double* matr_a = MemoryVectorMatrix(N);
+    double* matr_b = MemoryVectorMatrix(N);
+    double* matr_c = MemoryVectorMatrix(N);
 
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				matr_a[i * N + j] = ((double)i + 1);
-				matr_b[i * N + j] = ((double)i + 1);
-				matr_c[i * N + j] = (((double)i + 1) * 4);
-			}
-		}
-		double* matr_add4 = Add4(matr_a, matr_a, matr_b, matr_b, N);
-		for (int k = 0; k < N; k++)
-			for (int l = 0; l < N; l++)
-				ASSERT_TRUE(matr_c[k * N + l] == matr_add4[k * N + l]);
-	}
+    for (int i = 0; i < N; i++) {
+      for (int j = 0; j < N; j++) {
+        matr_a[i * N + j] = ((double)i + 1);
+        matr_b[i * N + j] = ((double)i + 1);
+        matr_c[i * N + j] = (((double)i + 1) * 4);
+      }
+    }
+    double* matr_add4 = Add4(matr_a, matr_a, matr_b, matr_b, N);
+    for (int k = 0; k < N; k++)
+      for (int l = 0; l < N; l++)
+        ASSERT_TRUE(matr_c[k * N + l] == matr_add4[k * N + l]);
+  }
 }
 
 TEST(strassen_algm_for_matrix, check_func_Sub_two_matrix) {
-	int size;
-	MPI_Comm_size(MPI_COMM_WORLD, &size);
-	if (size < 2) {
-		int N = 4;
-		double* matr_a = MemoryVectorMatrix(N);
-		double* matr_b = MemoryVectorMatrix(N);
-		double* matr_c = MemoryVectorMatrix(N);
+  int size;
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+  if (size < 2) {
+    int N = 4;
+    double* matr_a = MemoryVectorMatrix(N);
+    double* matr_b = MemoryVectorMatrix(N);
+    double* matr_c = MemoryVectorMatrix(N);
 
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				matr_a[i * N + j] = ((double)i + 1);
-				matr_b[i * N + j] = ((double)i + 1);
-				matr_c[i * N + j] = 0;
-			}
-		}
-		double* matr_sub2 = Sub2(matr_a, matr_b, N);
-		for (int k = 0; k < N; k++)
-			for (int l = 0; l < N; l++)
-				ASSERT_TRUE(matr_c[k * N + l] == matr_sub2[k * N + l]);
-	}
+    for (int i = 0; i < N; i++) {
+      for (int j = 0; j < N; j++) {
+        matr_a[i * N + j] = ((double)i + 1);
+        matr_b[i * N + j] = ((double)i + 1);
+        matr_c[i * N + j] = 0;
+      }
+    }
+    double* matr_sub2 = Sub2(matr_a, matr_b, N);
+    for (int k = 0; k < N; k++)
+      for (int l = 0; l < N; l++)
+        ASSERT_TRUE(matr_c[k * N + l] == matr_sub2[k * N + l]);
+  }
 }
 
 TEST(strassen_algm_for_matrix, check_func_Add_three_Sub_one_matrix) {
-	int size;
-	MPI_Comm_size(MPI_COMM_WORLD, &size);
-	if (size < 2) {
-		int N = 4;
-		double* matr_a = MemoryVectorMatrix(N);
-		double* matr_b = MemoryVectorMatrix(N);
-		double* matr_c = MemoryVectorMatrix(N);
+  int size;
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+  if (size < 2) {
+    int N = 4;
+    double* matr_a = MemoryVectorMatrix(N);
+    double* matr_b = MemoryVectorMatrix(N);
+    double* matr_c = MemoryVectorMatrix(N);
 
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				matr_a[i * N + j] = ((double)i + 1);
-				matr_b[i * N + j] = ((double)i + 1);
-				matr_c[i * N + j] = (((double)i + 1) * 3);
-			}
-		}
-		double* matr_add3sub1 = Add3Sub1(matr_a, matr_a, matr_b, matr_b, N);
-		for (int k = 0; k < N; k++)
-			for (int l = 0; l < N; l++)
-				ASSERT_TRUE(matr_c[k * N + l] == matr_add3sub1[k * N + l]);
-	}
+    for (int i = 0; i < N; i++) {
+      for (int j = 0; j < N; j++) {
+        matr_a[i * N + j] = ((double)i + 1);
+        matr_b[i * N + j] = ((double)i + 1);
+        matr_c[i * N + j] = (((double)i + 1) * 3);
+      }
+    }
+    double* matr_add3sub1 = Add3Sub1(matr_a, matr_a, matr_b, matr_b, N);
+    for (int k = 0; k < N; k++)
+      for (int l = 0; l < N; l++)
+        ASSERT_TRUE(matr_c[k * N + l] == matr_add3sub1[k * N + l]);
+  }
 }
 
 int main(int argc, char** argv) {
